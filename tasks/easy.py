91c001b9576d8c320594c09cedfa76eb653bab1d
Shanmugapriya Karthikumar <shanmugapriyaks5102005@gmail.com>
2026-04-09 20:56:53 +0530

    Update easy.py

--- tasks/easy.py
+++ tasks/easy.py
@@ -4,12 +4,27 @@ def run():
     env = DeliveryEnv()
     state = env.reset()
 
-    actions = ["go_to_restaurant", "pick_order", "go_to_customer", "deliver"]
+    actions = [
+        "select_order_1",
+        "go_to_restaurant",
+        "pick_order",
+        "go_to_customer",
+        "deliver",
+
+        "select_order_2",
+        "go_to_restaurant",
+        "pick_order",
+        "go_to_customer",
+        "deliver"
+    ]
 
     total_reward = 0
 
     for action in actions:
         state, reward, done = env.step(action)
         total_reward += reward
+
+        if done:
+            break
 
     return total_reward
